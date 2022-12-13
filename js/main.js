5f80a63bea1e311bb782dd3bdba843886c2f6cc9
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-12-13 17:49:12 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
     $('.main').fullpage({
         // navigation: true, 우측 중간에 도트로 위치를 알려줌
+        paddingTop: '80px',
         anchors: ['sec01', 'sec02', 'sec03', 'sec04'],
         afterLoad: function (anchorLink, index) {
             $('.header nav>ul>li').eq(index - 1).addClass('on').
@@ -12,11 +13,41 @@ $(function () {
 
     $('.mainSlide').slick({
         dots: false,
-        // autoplay: true,
+        arrows: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         pauseOnHover: false,
         pauseOnFocus: false,
         speed: 1000,
     });
 
+    $('.mainVisual .arrows .prev').on('click', function () {
+        $('.mainSlide').slick('slickPrev');
+    })
+    $('.mainVisual .arrows .next').on('click', function () {
+        $('.mainSlide').slick('slickNext');
+    })
+
+    $('.mainVisual .controller .play').on('click', function () {
+        $('.mainSlide').slick('slickPlay');
+    })
+    $('.mainVisual .controller .pause').on('click', function () {
+        $('.mainSlide').slick('slickPause');
+    })
+
+    var barWidth = $('.bar').width();
+    $('.mainSlide').on('init afterChange', function (e, s, c) {
+        var gauge = barWidth / s.slideCount;
+        $('.bar div').css({ width: gauge })
+        $('.bar div').css({ width: gauge * (c + 1) })
+    })
+    // mainVisual
+
+    $('.con_slide').slick({
+        arrows: false,
+    });
+
 })
+
+
+
